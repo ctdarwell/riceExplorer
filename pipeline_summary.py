5eb742ae7f7aa35f0f0af02fdf7f7095406667e2
evoclive <ctdarwell@gmail.com>
2021-09-15 10:07:19 +0700

    Add files via upload

---
+++ pipeline_summary.py
@@ -0,0 +1,48 @@
+import pandas as pd, glob, numpy as np, sys
+
+fldr = sys.argv[1]
+files = glob.glob1(fldr, '*_assocSNPs.csv') #assocSNPs is all extreme haplos
+in_grp = sys.argv[2].replace('_', ' ').split(',')
+
+hi, lo = pd.DataFrame(), pd.DataFrame()
+
+for f in files:
+    if '_lo_' in f: lo = pd.concat([lo, pd.read_csv(f"{fldr}/{f}", header=0)], axis=0)
+    if '_hi_' in f: hi = pd.concat([hi, pd.read_csv(f"{fldr}/{f}", header=0)], axis=0)
+
+try:
+    hi_nfv = hi[hi.type.isin(in_grp)]
+    hi_alt = hi.locus[~hi.type.isin(in_grp)].unique().tolist()
+except: pass
+
+try:
+    lo_nfv = lo[lo.type.isin(in_grp)]
+    lo_alt = lo.locus[~lo.type.isin(in_grp)].unique().tolist()
+except: pass
+
+try: hi_nfv[~hi_nfv.locus.isin(hi_alt)].drop_duplicates().to_csv(f'{fldr}/hiVal_uniqueResults.csv', index=False)
+except: pass
+try: lo_nfv[~lo_nfv.locus.isin(lo_alt)].drop_duplicates().to_csv(f'{fldr}/loVal_uniqueResults.csv', index=False)
+except: pass
+
+if not hi.empty:
+    hi = hi.drop_duplicates()
+    hi.to_csv(f"{fldr}/fullTable_highValGenos.csv", index=False)
+    hi = hi.acc.value_counts().reset_index().rename(
+           columns={'index': 'acc', 'acc': 'count'})
+    tmp = pd.DataFrame(np.array(['hi'] * hi.shape[0]).reshape(hi.shape[0],1), columns = list("X"))
+    hi = pd.concat([hi, tmp], axis = 1)
+
+if not lo.empty:
+    lo = lo.drop_duplicates()
+    lo.to_csv(f"{fldr}/fullTable_lowValGenos.csv", index=False)
+    lo = lo.acc.value_counts().reset_index().rename(
+           columns={'index': 'acc', 'acc': 'count'})
+    tmp = pd.DataFrame(np.array(['lo'] * lo.shape[0]).reshape(lo.shape[0],1), columns = list("X"))
+    lo = pd.concat([lo, tmp], axis = 1)
+
+df = pd.concat([hi, lo], axis = 0)
+
+df.columns = ['accession','count','phenotype']
+
+df.to_csv(f"{fldr}/ALL_accession_cnts.csv", index=False)
---
+++ riceExplorer.bash
@@ -0,0 +1,90 @@
+#!/bin/bash
+#SBATCH --cpus-per-task=24
+
+#module load SAMtools/1.12-GCC-10.2.0 BCFtools/1.12-GCC-10.2.0 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+source riceExplorer_vars #input parameters file
+rgd=$(cat $paths)
+
+#ADD ''' HERE IF NEEDED TO PART RUN WORFLOW:
+
+### *** START OF WORKFLOW ***
+mkdir $fldr
+rm $fldr/* #should be empty fldr as we write using '>>'
+
+bcftask(){
+while read p; #samples from samps
+do echo $(echo $p | rev | cut -d'/' -f 1 | rev | cut -d'.' -f 1) >> $3/$(echo $1 | cut -d',' -f 2).vcf
+bcftools view --min-ac=1  $p $(echo $1 | cut -d',' -f $4):$(echo $1 | cut -d',' -f $5)-$(echo $1 | cut -d',' -f $6) | sed -n '/AC=/,$p' >> $3/$(echo $1 | cut -d',' -f 2).vcf
+done < $2
+}
+
+export -f bcftask
+xargs -a $genes -rd'\n' -P20 -I'{}' bash -c 'bcftask "$1" "$2" "$3" "$4" "$5" "$6"' bash {} "$samps" "$fldr" "$chrom" "$first" "$end" #syntax note: {} represents $genes
+
+echo 1. DONE: bcftools-IRRI
+wait
+
+
+#rm repeat entries (i.e. from diff accs), format for snpEff & write allele X sample file: *.posn_samps.csv; sys.argv[2] denotes identifying prefix of sample name (to distinguish lines from rest of data)
+python vcf4snpEffLoop.py $gene_id*vcf $fldr $db_id #file search term, folder, sample prefix
+echo 2. DONE: vcf4snpEffLoop.py-1
+wait
+
+
+#run snpEff - id potentially influential SNPs
+snpefftask(){
+java -Xmx4g -jar /fs/home/cliveterence.dar/snpEff/snpEff.jar $2 $1 > $(echo $1 | cut -d'.' -f 1).ann.vcf
+#select only loci with significant predicted impact
+grep 'MODERATE' $(echo $1 | cut -d'.' -f 1).ann.vcf >> $(echo $1 | cut -d'.' -f 1).ann.best
+grep 'HIGH' $(echo $1 | cut -d'.' -f 1).ann.vcf >> $(echo $1 | cut -d'.' -f 1).ann.best
+}
+
+export -f snpefftask
+find $fldr/ -name $gene_id*vcf | xargs -P20 -I'{}' -d'\n' bash -c 'snpefftask "$1" "$2"' bash {} $db #searches for all LOC*vcf files
+
+rm $fldr/$gene_id*.ann.vcf
+find $fldr/*best -size 0 -delete
+echo 3. DONE: snpEff
+wait
+
+#evaluate which IRRI haplotypes (based on snpEff influential SNPs), have extremes phenotypes 
+python hapXphenoPredictorSNPsLoop.py $data $fldr $percentiles
+echo 4. DONE: hapXphenoPredictorSNPs.py - checked haps!
+wait
+
+
+#search RGD for ALT alleles in focal gene region: OUT: rgd_*.vcf
+bcftask2(){
+q=$(grep $(echo $1 | cut -d'/' -f 2 | cut -d'.' -f 1) $3) #$3 = $genes
+for r in $2
+do echo $(echo $r | rev | cut -d'/' -f 1 | rev | cut -d'.' -f 1) >> $4/rgd_$(echo $q | cut -d',' -f $8).vcf
+bcftools view --min-ac=1 $r $(echo $q | cut -d',' -f $5):$(echo $q | cut -d',' -f $6)-$(echo $q | cut -d',' -f $7) | sed -n '/AC=/,$p' >> $4/rgd_$(echo $q | cut -d',' -f $8).vcf
+done
+}
+
+export -f bcftask2
+find $fldr/ -name "*haps.csv" | xargs -P20 -I'{}' -d'\n' bash -c 'bcftask2 "$1" "$2" "$3" "$4" "$5" "$6" "$7" "$8"' bash {} "$rgd" "$genes" "$fldr" "$chrom" "$first" "$end" "$loc_col"
+
+echo 5. DONE: bcftools-RGD
+wait
+
+
+#format bcftools file
+python vcf4snpEffLoop.py rgd_$gene_id*vcf $fldr $focal_id #file search term, folder, sample prefix
+echo 6. DONE: vcf4snpEffLoop.py-2
+
+
+#look for SNPs coding extreme phenotypes in IRRI data (e.g. hi or lo) that are present in landraces/RD lines that are not found in RGD varieties
+python potential_rgd_sourcesLoop.py $fldr $accs $fig1 #OUT: *potential*donors.csv & *assocSNPs.csv
+echo 7. DONE: potential_rgd_sources.py
+
+
+#make figure - NB pipelineGraphOut_bashLoop.py
+python pipelineGraphOut.py $fldr $all_msu $idd_genes $gene_id $fig1 $fig2 $chrom $loc_col $first $end $nChroms
+echo 8. DONE: mk figure!
+
+#summarise output files
+python pipeline_summary.py $fldr $fig1
+echo finito!
+
